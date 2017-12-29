3a4be6d0a9adbed4ac447a72948c5b6612128aea
David Huie <dahuie@gmail.com>
2017-12-28 17:11:12 -0800

    Fix commenbt

--- pkg/anomaly/zscore/extractors/flapping_node.go
+++ pkg/anomaly/zscore/extractors/flapping_node.go
@@ -4,7 +4,7 @@ import "github.com/dollarshaveclub/node-auto-repair-operator/pkg/nodes"
 
 // FlappingNode is a FeatureExtractor that tries to highlight
 // instability by capturing the number of times that a Kubernetes node
-// alternates between a Ready/NotReady status.
+// alternates between Ready/NotReady statuses.
 type FlappingNode struct{}
 
 func (f *FlappingNode) Extract(ns *nodes.NodeTimePeriodSummary) (float64, error) {
