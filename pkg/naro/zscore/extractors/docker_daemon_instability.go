4aa0538c710fd0e1c672bd01f333d95ec40bf952
David Huie <dahuie@gmail.com>
2018-01-10 15:04:41 -0800

    Instability feature extractor

---
+++ pkg/naro/zscore/extractors/docker_daemon_instability.go
@@ -0,0 +1,54 @@
+package extractors
+
+import "github.com/dollarshaveclub/node-auto-repair-operator/pkg/naro"
+
+// DockerDaemonInstability is a zscore.FeatureExtractor that tries to
+// highlight Docker daemon instability on a Kubernetes node by
+// capturing the number of times instability is detected.
+type DockerDaemonInstability struct{}
+
+// NewDockerDaemonInstability returns a new instance of
+// DockerDaemonInstability.
+func NewDockerDaemonInstability() *DockerDaemonInstability {
+	return &DockerDaemonInstability{}
+}
+
+// Extract returns the number of times Docker daemon instability is
+// detected within the naro.NodeTimePeriodSummary.
+func (f *DockerDaemonInstability) Extract(ns *naro.NodeTimePeriodSummary) (float64, error) {
+	instabilityPeriods := 0
+	unstable := false
+
+	// This loop iterates through all events looking for periods
+	// of time where a node experienced a NodeNotReady event
+	// followed by a ContainerGCFailed event. Via experimentation,
+	// we found that these events align with Docker daemon issues.
+	for _, event := range ns.Events {
+		if event.Reason == "NodeNotReady" &&
+			event.SourceComponent == "controllermanager" {
+			unstable = true
+			continue
+		}
+		if unstable {
+			// ContainerGCFailed confirms that the
+			// situation is a Docker daemon issue.
+			if event.Reason == "ContainerGCFailed" &&
+				event.SourceComponent == "kubelet" {
+				instabilityPeriods++
+				unstable = false
+				continue
+			}
+
+			// If the node is now ready, we did not detect
+			// a Docker daemon issue, so we start from
+			// scratch.
+			if event.Reason == "NodeReady" &&
+				event.SourceComponent == "kubelet" {
+				unstable = false
+				continue
+			}
+		}
+	}
+
+	return float64(instabilityPeriods), nil
+}
---
+++ pkg/naro/zscore/extractors/docker_daemon_instability_test.go
@@ -0,0 +1,106 @@
+package extractors_test
+
+import (
+	"testing"
+
+	"github.com/dollarshaveclub/node-auto-repair-operator/pkg/naro"
+	"github.com/dollarshaveclub/node-auto-repair-operator/pkg/naro/zscore/extractors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDockerDaemonInstabilityExtract(t *testing.T) {
+	extractor := extractors.NewDockerDaemonInstability()
+
+	t.Run("nothing", func(t *testing.T) {
+		tps := &naro.NodeTimePeriodSummary{}
+		feature, err := extractor.Extract(tps)
+		if assert.NoError(t, err) {
+			assert.Zero(t, feature)
+		}
+	})
+
+	t.Run("partial", func(t *testing.T) {
+		tps := &naro.NodeTimePeriodSummary{
+			Events: []*naro.NodeEvent{
+				&naro.NodeEvent{
+					Reason:          "NodeNotReady",
+					SourceComponent: "controllermanager",
+				},
+			},
+		}
+		feature, err := extractor.Extract(tps)
+		if assert.NoError(t, err) {
+			assert.Zero(t, feature)
+		}
+	})
+
+	t.Run("full", func(t *testing.T) {
+		tps := &naro.NodeTimePeriodSummary{
+			Events: []*naro.NodeEvent{
+				&naro.NodeEvent{
+					Reason:          "NodeNotReady",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "ContainerGCFailed",
+					SourceComponent: "kubelet",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+			},
+		}
+		feature, err := extractor.Extract(tps)
+		if assert.NoError(t, err) {
+			assert.Equal(t, float64(1), feature)
+		}
+	})
+
+	t.Run("multiple", func(t *testing.T) {
+		tps := &naro.NodeTimePeriodSummary{
+			Events: []*naro.NodeEvent{
+				&naro.NodeEvent{
+					Reason:          "NodeNotReady",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "ContainerGCFailed",
+					SourceComponent: "kubelet",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "NodeNotReady",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+				&naro.NodeEvent{
+					Reason:          "ContainerGCFailed",
+					SourceComponent: "kubelet",
+				},
+				&naro.NodeEvent{
+					Reason:          "Test",
+					SourceComponent: "controllermanager",
+				},
+			},
+		}
+		feature, err := extractor.Extract(tps)
+		if assert.NoError(t, err) {
+			assert.Equal(t, float64(2), feature)
+		}
+	})
+}
