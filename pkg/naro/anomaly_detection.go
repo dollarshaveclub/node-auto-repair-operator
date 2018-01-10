502b8cc272d5bc4dc3de6a3f3fc1ea756c2f8baa
David Huie <dahuie@gmail.com>
2018-01-10 15:11:20 -0800

    Add anomaly detection base types

--- pkg/naro/anomaly_detection.go
+++ pkg/naro/anomaly_detection.go
@@ -1,1 +1,8 @@
 package naro
+
+// AnomalyDetector is a type that can be trained to detect issues
+// within new NodeTimePeriodSummaries.
+type AnomalyDetector interface {
+	Train(summaries <-chan *NodeTimePeriodSummary, done chan<- struct{}) error
+	IsAnomaly(ns *NodeTimePeriodSummary) (bool, error)
+}
